df4906dbd03d92e8756f9219cdc032bb75c26630
Arbaz Ali <arbaz.ali@dotnova.ai>
2024-07-14 14:45:54 +0530

    Added new router file

--- routes/userRouter.go
+++ routes/userRouter.go
@@ -3,11 +3,10 @@ package routes
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt-project/controllers"
-	"github.com/golang-jwt-project/middlewares"
 )
 
 func UserRoutes(incomingRoutes *gin.Engine) {
-	incomingRoutes.Use(middlewares.Authenticate())
+	//incomingRoutes.Use(middlewares.Authenticate())
 	incomingRoutes.GET("/users", controllers.GetUsers())
 	incomingRoutes.GET("/users/:user_id", controllers.GetUser())
 }
